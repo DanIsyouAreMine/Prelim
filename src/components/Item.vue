5d9f7b30cb17a4d4fa5d5bb2a254ae454f39658f
DanIsyouAreMine <danchristianaguillon9@gmail.com>
2024-09-11 15:23:35 +0800

    Prelim

---
+++ src/components/Item.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ item.name }} - ${{ item.price }}</p>
+    <button @click="addToCart">Add to Cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.item);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Item List</h1>
+    <ul>
+      <li v-for="item in filteredItems" :key="item.id">
+        <Item :item="item" @add-to-cart="handleAddToCart" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  components: {
+    Item,
+  },
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'Apple', price: 1.00, isVisible: true },
+        { id: 2, name: 'Banana', price: 0.75, isVisible: false },
+        { id: 3, name: 'Cherry', price: 2.00, isVisible: true },
+      ],
+    };
+  },
+  computed: {
+    filteredItems() {
+      return this.items.filter(item => item.isVisible);
+    },
+  },
+  methods: {
+    handleAddToCart(item) {
+      console.log(`Added to cart: ${item.name} - $${item.price}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
